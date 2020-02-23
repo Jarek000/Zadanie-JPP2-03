d46fc4adc837ef9c6b759a246153c4f6487dd6b7
Maciej Czulak <maciejczulak@o2.pl>
2020-02-23 15:21:54 +0100

    yo2

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -2,11 +2,12 @@ package pl.sda;
 
 public class Main {
     public static void bdf () {
+        
         System.out.println("bdf");
     }
 
     public static void main(String[] args) {
-        
+
 
     }
 }
