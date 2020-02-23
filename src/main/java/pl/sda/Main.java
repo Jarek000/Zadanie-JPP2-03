45b7d378066e1428a4d114c21778e5d8acb98fe5
Your Name <you@example.com>
2020-02-23 15:18:40 +0100

    dodany kod

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -1,7 +1,20 @@
 package pl.sda;
 
+import java.util.Scanner;
+
 public class Main {
+    public  static  void  bdf () {
+        System . out . println ( "bdf" );
+    }
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Wpisz ciąg: ");
+        char[] litery = scanner.nextLine().toCharArray();
+        System.out.print("Odwrócony ciąg: ");
+        for (int i = litery.length - 1; i >= 0; i--) {
+            System.out.print(litery[i]);
+
+        }
+        System.out.print("\n");
     }
 }
