e793f78b47b21f9eee6be7870af430e8b08398cc
Maciej Czulak <maciejczulak@o2.pl>
2020-02-23 15:09:37 +0100

    moja czesc roboty

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -1,9 +1,12 @@
 package pl.sda;
 
 public class Main {
+    public static void bdf () {
+        System.out.println("bdf");
+    }
+
     public static void main(String[] args) {
-        int a = 1;
-        System.out.println(a);
         
+
     }
 }
